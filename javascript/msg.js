f02efb1209319c5cdb28840d57cb6aa17e157732
Sasiprakash-Sparkout <sasiprakash@sparkouttech.com>
2026-08-07 05:55:19 +0530

    file added

---
+++ javascript/msg.js
@@ -0,0 +1,11 @@
+let form =document.getElementById("taskform");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
+let task=getElementById("task");
+let mes=getElementById("message");
+if(task===""){
+
+
+}
